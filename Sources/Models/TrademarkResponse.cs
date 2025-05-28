3ea8d80938922d75c5fef13e2f49a44bd6cd2bef
Vadim Belov <github@belov.us>
2025-05-27 20:48:13 -0700

    Implement trademark checking functionality
    Updated `TrademarkChecker.cs` to add the `CheckTrademarkAsync` method, which verifies trademark availability by checking for existing `.com` domains and U.S. registrations. Introduced private methods for domain and trademark checks, including API integration with the USPTO.
    
    Added `TrademarkResponse.cs` to define the structure of the USPTO API response, including classes for deserializing JSON data and handling trademark details.

---
+++ Sources/Models/TrademarkResponse.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TrademarkOfficer.Models
+{
+    internal class TrademarkResponse
+    {
+        [JsonPropertyName("hits")]
+        public HitResult Result { get; set; } = null!;
+
+        public class HitResult
+        {
+            [JsonPropertyName("hits")]
+            public IEnumerable<Hit> Hits { get; set; } = [];
+        }
+
+        public class Hit
+        {
+            [JsonPropertyName("score")]
+            public float Score { get; set; }
+
+            [JsonPropertyName("source")]
+            public Source SourceInfo { get; set; } = null!;
+        }
+
+        public class Source
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; set; } = string.Empty;
+
+            [JsonPropertyName("alive")]
+            public bool Alive { get; set; }
+
+            [JsonPropertyName("filedDate")]
+            public DateTime FiledDate { get; set; }
+
+            [JsonPropertyName("goodsAndServices")]
+            public IEnumerable<string> GoodsAndServices { get; set; } = [];
+
+            [JsonPropertyName("markType")]
+            public IEnumerable<string> MarkTypes { get; set; } = [];
+
+            [JsonPropertyName("ownerName")]
+            public IEnumerable<string> OwnerNames { get; set; } = [];
+
+            [JsonPropertyName("ownerFullText")]
+            public IEnumerable<string> OwnerFullTexts { get; set; } = [];
+
+            [JsonIgnore]
+            public string OwnerName => OwnerNames.Any() ? string.Join(", ", OwnerNames) : string.Empty;
+        }
+    }
+}
--- Sources/TrademarkChecker.cs
+++ Sources/TrademarkChecker.cs
@@ -1,4 +1,6 @@
 ﻿using Serilog;
+using System.Net.Http.Json;
+using TrademarkOfficer.Models;
 
 namespace TrademarkOfficer
 {
@@ -6,7 +8,67 @@ namespace TrademarkOfficer
     {
         internal static async Task CheckTrademarkAsync(string trademarkName, ILogger logger)
         {
-            throw new NotImplementedException();
+            bool hasComDomain = await CheckDomainAsync(trademarkName + ".com");
+            bool isRegisteredInUs = await CheckTrademarkInUsAsync(trademarkName, logger);
+            if (hasComDomain || isRegisteredInUs)
+            {
+                if (hasComDomain)
+                {
+                    logger.Warning("Trademark '{TrademarkName}' already has a .com domain registered.", trademarkName);
+                }
+                if (isRegisteredInUs)
+                {
+                    logger.Warning("Trademark '{TrademarkName}' is already registered in the US.", trademarkName);
+                }
+            }
+            else
+            {
+                logger.Information("Trademark '{TrademarkName}' is available for registration and does not have a .com domain.", trademarkName);
+            }
+        }
+
+        private static async Task<bool> CheckDomainAsync(string domainName)
+        {
+            string url = "https://rdap.godaddy.com/v1/domain/" + domainName.ToUpperInvariant();
+            using HttpClient client = new();
+            var response = await client.GetAsync(url);
+            return response.IsSuccessStatusCode;
+        }
+
+        private static async Task<bool> CheckTrademarkInUsAsync(string trademarkName, ILogger logger)
+        {
+            const int minScore = 10;
+            const string url = "https://tmsearch.uspto.gov/api-v1-0-0/tmsearch";
+            const string queryTemplate = "{\"query\":{\"bool\":{\"must\":[{\"bool\":{\"should\":[{\"query_string\":{\"query\":\"{{Trademark}}*\"," +
+                "\"default_operator\":\"AND\",\"fields\":[\"goodsAndServices\",\"markDescription\",\"ownerName\",\"translate\"," +
+                "\"wordmark\",\"wordmarkPseudoText\"]}},{\"term\":{\"WM\":{\"value\":\"{{Trademark}}\",\"boost\":6}}},{\"match_phrase\":" +
+                "{\"WMP5\":{\"query\":\"{{Trademark}}\"}}},{\"query_string\":{\"query\":\"{{Trademark}}\",\"default_operator\":\"AND\",\"fields\":" +
+                "[\"goodsAndServices\",\"markDescription\",\"ownerName\",\"translate\",\"wordmark\",\"wordmarkPseudoText\"]}}," +
+                "{\"term\":{\"SN\":{\"value\":\"{{Trademark}}\"}}},{\"term\":{\"RN\":{\"value\":\"{{Trademark}}\"}}}]}}],\"filter\":[{\"term\":" +
+                "{\"LD\":\"true\"}}]}},\"aggs\":{\"alive\":{\"terms\":{\"field\":\"alive\"}},\"cancelDate\":{\"value_count\":" +
+                "{\"field\":\"cancelDate\"}}},\"size\":100,\"from\":0,\"track_total_hits\":true,\"highlight\":{\"fields\":{" +
+                "\"abandonDate\":{},\"attorney\":{},\"alive\":{},\"cancelDate\":{},\"coordinatedClass\":{},\"currentBasis\":{}," +
+                "\"drawingCode\":{},\"filedDate\":{},\"goodsAndServices\":{},\"id\":{},\"internationalClass\":{},\"markDescription\":{}," +
+                "\"markType\":{},\"ownerFullText\":{},\"ownerName\":{},\"ownerType\":{},\"priorityDate\":{},\"registrationDate\":{}," +
+                "\"registrationId\":{},\"registrationType\":{},\"supplementalRegistrationDate\":{},\"translate\":{},\"usClass\":{}," +
+                "\"wordmarkPseudoText\":{}},\"pre_tags\":[\"<strong>\"],\"post_tags\":[\"</strong>\"],\"number_of_fragments\":0}," +
+                "\"_source\":[\"abandonDate\",\"alive\",\"attorney\",\"cancelDate\",\"coordinatedClass\",\"currentBasis\",\"drawingCode\"," +
+                "\"filedDate\",\"goodsAndServices\",\"id\",\"internationalClass\",\"markDescription\",\"markType\",\"ownerFullText\"," +
+                "\"ownerName\",\"ownerType\",\"priorityDate\",\"registrationDate\",\"registrationId\",\"registrationType\"," +
+                "\"supplementalRegistrationDate\",\"translate\",\"usClass\",\"wordmark\",\"wordmarkPseudoText\"],\"min_score\":{{MinScore}}}";
+            string request = queryTemplate.Replace("{{Trademark}}", trademarkName).Replace("{{MinScore}}", minScore.ToString());
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36");
+            var response = await client.PostAsJsonAsync(url, request);
+            TrademarkResponse result = await response.Content.ReadFromJsonAsync<TrademarkResponse>()
+                ?? throw new InvalidOperationException("Failed to deserialize the trademark response.");
+            foreach (var hit in result.Result.Hits)
+            {
+                logger.Debug("Found trademark: {TrademarkId} with score {Score}: {Name}", hit.SourceInfo.Id, hit.Score, hit.SourceInfo.OwnerName);
+            }
+            return result.Result.Hits.Any();
         }
     }
+
+
 }
